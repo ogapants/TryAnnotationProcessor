14c4735e8dcaf30193a92ad906a5eb93b7716bf3
ogapants <ogapamyu@gmail.com>
2018-09-24 21:47:05 +0900

    <<<ADD LOST PARAMS>>>

--- annotation/src/main/java/com/github/ogapants/annotation/YamBuilder.kt
+++ annotation/src/main/java/com/github/ogapants/annotation/YamBuilder.kt
@@ -1,9 +1,5 @@
 package com.github.ogapants.annotation
 
-import java.lang.annotation.Retention
-import java.lang.annotation.Target
-
-
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class YamBuilder
